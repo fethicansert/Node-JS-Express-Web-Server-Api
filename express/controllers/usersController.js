0cc54d4015e7d1fc459cddb6d620bfc97f683511
fethi <fethisert0@gmail.com>
2024-04-21 19:22:24 +0300

    kullanicilarin kendi calimam gerek sorularinu eklemesi icin route API eklendi

--- express/controllers/usersController.js
+++ express/controllers/usersController.js
@@ -2,12 +2,39 @@ const User = require('../model/User');
 
 const getAllUsers = async (req, res) => {
     const users = await User.find({}).exec();
-    if(!users) return res.json({message: "No users founeded."});
+    if (!users) return res.json({ message: "No users founeded." });
     res.json(users);
+}
+
+
+const addQuestion = async (req, res) => {
+
+
+    const { user, questionId } = req.body;
+
+    if (!user || !questionId) return res.status(400).json({ error: "Something Missing !" });
+
+    const foundUser = await User.findOne({ username: user }).exec();
+
+    if (!foundUser) return res.sendStatus(401);
+
+    //Check if question already in the users quesitons list
+    const duplicate = foundUser.questions.find(currentQuestionID => currentQuestionID === questionId); 
+    if(duplicate) return res.status(409).json({error: 'Question alreday in the study list !'});
+
+    try {
+        foundUser.questions.push(questionId);
+        const result = await foundUser.save();
+        console.log(result);
+    } catch (err) {
+        console.log(err);
+        res.sendStatus(400);
+    }
+   
+    return res.sendStatus(200);
 }
 
 
 
 
-
-module.exports = { getAllUsers }
+module.exports = { getAllUsers, addQuestion }
